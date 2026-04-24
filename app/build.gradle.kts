e688ae2791f46eedd29fce73338e3a5afdf03007
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2026-04-24 12:28:20 +0100

    Add initial project setup with AndroidManifest, Gradle configuration, and resource files
    Signed-off-by: Jeffery Orazulike <chukwudumebiorazulike@gmail.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "dev.logickoder.ragwithgemma"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "dev.logickoder.ragwithgemma"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.core)
+    implementation(libs.core.appcompat)
+    implementation(libs.core.material)
+
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    implementation(libs.sqlite.vss.android)
+
+    // Local AI Runtimes
+    implementation(libs.ai.edge.litert)
+    implementation(libs.mediapipe.tasks.text)
+
+    // Kotlin
+    implementation(libs.kotlin.json)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.espresso.core)
+}
